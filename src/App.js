ffb2fd9ffa33b7be5f2f4a0e9f72c65909533504
brit-starks <brittneytechiethomas@gmail.com>
2019-07-30 20:24:06 -0700

    Make API Call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
   return (
+    
     <div className="App">
+
+    const [nasaData, setNasaData] = useState([]);
+    useEffect(() => {
+      
+      axios.get("https://dog.ceo/api/breed/hound/images/random/25")
+      .then(res => {
+        console.log(res)}
+      )}, []);
+
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        {/* Read through the instructions in the README.md file to build your NASA
+        app! Have fun <span role=img>🚀</span>! */}
       </p>
     </div>
   );
