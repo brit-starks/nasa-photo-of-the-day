bc952f30a68a5986708a2d3cfbd280f4eb15af14
brit-starks <brittneytechiethomas@gmail.com>
2019-08-01 07:52:07 -0700

    Correct image prop

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -5,7 +5,7 @@ const Photo = (props) => {
     <div className="photo-container">
       <h3>{props.title}</h3>
       <div className="image">
-        <img src={props.image} alt="Photo of the day" />
+        <img src={props.img} alt="Photo of the day" />
       </div> 
     </div>
   )
